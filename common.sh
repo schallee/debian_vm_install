4ef972179db351c8ea54ad8b315d85b31979ba6e
Ed Schaller <schallee@darkmist.net>
2020-08-07 08:03:09 -0600

    library stuff

--- common.sh
+++ common.sh
@@ -13,11 +13,102 @@ DEBIAN_NETBOOT_INITRAMFS="${DEBIAN_NETBOOT}/${DEBIAN_NETBOOT_INITRAMFS_FILE}"
 DEBIAN_INSTALLER_SHA256_FILE="SHA256SUMS"
 DEBIAN_INSTALLER_SHA256="${DEBIAN_INSTALLER_IMAGES_HTTPS}/${DEBIAN_INSTALLER_SHA256_FILE}"
 #https://deb.debian.org/debian//dists/stable/main/installer-amd64/current/images/netboot/debian-installer/amd64/initrd.gz
+WORK_DIR=''
 
 die()
 {
 	echo "$@" >&2
 	exit 1
+}
+
+warn()
+{
+	echo "$@" >&2;
+}
+
+debug()
+{
+	return 0;
+}
+
+debugf()
+{
+	return 0;
+}
+
+is_debug_enabled()
+{
+	return 1;
+}
+
+debug_enable()
+{
+	debug()
+	{
+		echo "$@" >&2
+	}
+
+	debugf()
+	{
+		printf "$@" >&2
+	}
+
+	is_debug_enabled()
+	{
+		return 0;
+	}
+}
+
+verbose()
+{
+	return 0;
+}
+
+verbosef()
+{
+	return 0;
+}
+
+is_verbose_enabled()
+{
+	return 0;
+}
+
+verbose_enable()
+{
+	verbose()
+	{
+		echo "$@" >&2;
+	}
+
+	verbosef()
+	{
+		printf "$@" >&2;
+	}
+
+	is_verbose_enabled()
+	{
+		return 0;
+	}
+}
+
+cleanup_work_dir()
+{
+	cd / >/dev/null 2>&1;
+	[ -d "${WORK_DIR}" ] || return;
+	verbosef 'Removing work directory "%s"...' "${WORK_DIR}";
+	rm -fR "${WORK_DIR}" || warn "Removal of temporary directory ${WORK_DIR} returned error.";
+	verbose 'done.';
+	[ -d "${WORK_DIR}" ] && die "Temporary directory ${WORK_DIR} still exists. Manual cleanup needed.";
+}
+
+create_work_dir()
+{
+	trap cleanup_work_dir 0 || die "Failed to setup work directory cleanup trap.";
+	verbosef 'Creating work directory...'
+	WORK_DIR=`mktemp -dt "${BASENAME}.XXXXXXXXXX.tmp"`;
+	[ -d "${WORK_DIR}" ] || die "mktemp did not create work directory.";
+	verbose "${WORK_DIR}."
 }
 
 sha256()
@@ -44,7 +135,43 @@ sha256()
 		# SHA256(initrd.gz)= d9d36ef8242f15848b608ffac862eb1d42965c0222910bf29bb129f8a377626f
 	# fi
 	die "Unable to find sha256sum varient."
-	
+}
+
+download_missing_file()
+{
+	local file
+	local url
+
+	file="${1}"
+	url="${2}"
+
+	verbosef 'Checking for %s...' "${file}"
+	if [ -f "${file}" ]; then
+		verbose "present."
+		return;
+	fi
+	verbose "missing."
+	verbosef 'Downloading %s:\n' "${url}"
+	curl -# -o "${file}" -C - "${url}" || die "Failed to download ${url}"
+	verbosef 'Downloading %s: done.\n' "${url}"
+}
+
+check_sha256()
+{
+	local expected
+	local file
+	local actual
+
+	expected="${1%% *}"	# clean any space filename from the end
+	file="${2}"
+	verbosef 'Checking sha256 of %s...' "${file}"
+	actual=`sha256 "${file}" | cut -d' ' -f1`
+
+	if [ x"${expected}" = x"${actual}" ]; then
+		verbose 'correct.'
+	else
+		die "Expected ${expected} but got ${actual}"
+	fi
 }
 
 # based on
--- repkg_initramfs.sh
+++ repkg_initramfs.sh
@@ -1,55 +1,49 @@
 #! /bin/sh
 
-set -e
+set -e;
 
-. `dirname "$0"`/common.sh
+BASEDIR=`dirname "$0"`;
+BASENAME=`basename "$0"`;
+. "${BASEDIR}/common.sh";
 
-download_missing_file()
+usage()
 {
-	local file
-	local url
-
-	file="${1}"
-	url="${2}"
-
-	printf 'Checking for %s...' "${file}"
-	if [ -f "${file}" ]; then
-		echo "present."
-		return;
-	fi
-	echo "missing."
-	printf 'Downloading %s:\n' "${url}"
-	curl -o "${file}" -C - "${url}}" || die "Failed to download ${url}"
-	printf 'Downloading %s: done.\n' "${url}"
+	[ -n "$*" ] && warn "$@";
+	die "usage: ${BASENAME} [-dvx]";
 }
 
-check_sha256()
-{
-	local expected
-	local file
-	local actual
+# Parse arguments
+opt=
+while getopts dvx opt; do
+	case $opt in
+		x)
+			set -x;
+			;;
+		d)
+			debug_enable;
+			;;
+		v)
+			verbose_enable;
+			;;
+		*)
+			usage "Unknown option \"${opt}\".";
+			;;
+	esac
+done
 
-	expected="${1%% *}"	# clean any space filename from the end
-	file="${2}"
-	printf 'Checking sha256 of %s...' "${file}"
-	actual=`sha256 "${file}" | cut -d' ' -f1`
+shift `expr $OPTIND - 1`
 
-	if [ x"${expected}" = x"${actual}" ]; then
-		echo 'correct.'
-	else
-		die "Expected ${expected} but got ${actual}"
-	fi
-}
+create_work_dir
 
-download_missing_file "${DEBIAN_NETBOOT_INITRAMFS_FILE}" "${DEBIAN_NETBOOT_INITRAMFS}"
-download_missing_file "${DEBIAN_INSTALLER_SHA256_FILE}" "${DEBIAN_INSTALLER_SHA256}"
-initramfs_sha256_regex='^[0-9a-zA-Z]{64}  '$(escape_for_regex "./${DEBIAN_NETBOOT_DIR}/${DEBIAN_NETBOOT_INITRAMFS_FILE}")'$'
-initramfs_sha256=$(grep -E "${initramfs_sha256_regex}" "${DEBIAN_INSTALLER_SHA256_FILE}")
-initramfs_sha256="${initramfs_sha256%% *}"
-check_sha256 "${initramfs_sha256}" "${DEBIAN_NETBOOT_INITRAMFS_FILE}"
+download_missing_file "${DEBIAN_NETBOOT_INITRAMFS_FILE}" "${DEBIAN_NETBOOT_INITRAMFS}";
+download_missing_file "${DEBIAN_INSTALLER_SHA256_FILE}" "${DEBIAN_INSTALLER_SHA256}";
+initramfs_sha256_regex='^[0-9a-zA-Z]{64}  '$(escape_for_regex "./${DEBIAN_NETBOOT_DIR}/${DEBIAN_NETBOOT_INITRAMFS_FILE}")'$';
+initramfs_sha256=$(grep -E "${initramfs_sha256_regex}" "${DEBIAN_INSTALLER_SHA256_FILE}");
+initramfs_sha256="${initramfs_sha256%% *}";
+check_sha256 "${initramfs_sha256}" "${DEBIAN_NETBOOT_INITRAMFS_FILE}";
+
 #check_sha256 $(grep -E '^[0-9a-zA-Z]{64}  '$(escape_for_regex "./${DEBIAN_NETBOOT_DIR}/${DEBIAN_NETBOOT_INITRAMFS_FILE}")'$' "${DEBIAN_INSTALLER_SHA256_FILE}" | cut -d' ' -f1) "${DEBIAN_NETBOOT_INITRAMFS_FILE}"
 
- cpio -i -d -H newc -F initramfs_data.cpio --no-absolute-filenames
+# cpio -i -d -H newc -F initramfs_data.cpio --no-absolute-filenames
 
-find . | cpio -o -H newc | gzip
-
+# find . | cpio -o -H newc | gzip
